04b5e6d6811449b769212a3415aae49cb7d9fb71
Ilya Markov <imarkovkin@gmail.com>
2022-11-28 22:22:41 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lab_23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            AsyncFactorial(n);
+            Console.ReadKey();
+        }
+
+        static void Factorial(int n)
+        {
+            int s = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                s *= i;
+                Thread.Sleep(10);
+            }
+            Console.WriteLine(s);
+
+        }
+
+        static async void AsyncFactorial(int n)
+        {
+            await Task.Run(() => Factorial(n));
+        }
+    }
+    
+}
